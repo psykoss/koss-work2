29af30aac9dccef9f84a70312382e3b74e2c99f1
psykoss115 <egomirror115@gmail.com>
2022-08-02 22:27:21 +0200

    KOSS.PY ADDED

---
+++ programming/KOSS.py
@@ -0,0 +1,47 @@
+var = input('WELCOME KOSS ,PRESS ENTER TO START:')
+i=1
+o= " "
+
+if ("") in var:
+ def koss():
+    Fk_1 = [1,1,1,0,1,1,1,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,0,1,1]
+    Fk_3 = [1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,1]
+    Fk_4 = [1,1,1,1,1,1,0,0,0,1,0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,0,0,1,1]
+    Fk_5 = [1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0,0,0,0,0,0,3,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0]
+
+    for i in Fk_1:
+        if i == 0:
+            print(o, end="")
+        print(i*"1", end="")
+    print()
+    for i in Fk_1:
+        if i == 0:
+            print(o, end="")
+        print(i*"1", end="")
+    print()
+    for i in Fk_3:
+        if i == 0:
+            print(o, end="")
+        print(i*"1", end="")
+    print()
+    for i in Fk_4:
+        if i == 0:
+            print(o, end="")
+        print(i*"1", end="")
+    print()
+    for i in Fk_5:
+        if i == 0:
+            print(o, end="")
+        print(i*"1", end="")
+    print()
+    for i in Fk_1:
+        if i == 0:
+            print(o, end="")
+        print(i*"1", end="")
+    print()
+    for i in Fk_1:
+        if i == 0:
+            print(o, end="")
+        print(i*"1", end="")
+    print()
+koss()
